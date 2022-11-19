5a19a4cf710a9cfbd3b5d520bd8ec12ce14390be
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2022-11-19 23:00:27 +0700

    travel

---
+++ about.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!--swiper css link-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+
+
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <!--custom css file link-->
+    <link rel="stylesheet" href="./assest8/css/style.css">
+
+    <!--custom css responsive file link-->
+    <link rel="stylesheet" href="./assest8/css/responsive.css">
+</head>
+<body>
+    
+<!--header section starts-->
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel</a>
+
+    <nav class="navbar">
+        <ul>
+            <li>
+                <a href="home.php">home</a>
+            </li>
+            <li>
+                <a href="about.php">about</a>
+            </li>
+            <li>
+                <a href="package.php">package</a>
+            </li>
+            <li>
+                <a href="book.php">book</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!--header section ends-->
+
+<div class="heading" style="background: url(assest8/images/abstract-fluid-blue-wave-banner-background-illustration-vector.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts-->
+
+<section class="about">
+
+    <div class="image">
+        <img src="./assest8/images/contiki-hp-hot-air-balloon-selfie-0277amer2019.jpg" alt="">
+    </div>
+   
+    <div class="content">
+        <h3>why choose us</h3>
+        <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit . Iure numquam null iusto corporis commodi libero, vitae obcaecati optio rerum ab 
+            culpa nesciunt, earum mollitia quasi ipsam non. Aliquid, iure.
+        </p>
+        <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit . Aliquid rerum, deletus voluptate aliquam
+            quaerat iusto repellendus error nulla ab atque
+        </p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide service</span>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- about section ends-->
+
+<!-- reviews section starts-->
+
+<section class="reviews">
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                Lorem ipsum dolor , sit amet consectetur adipisicing elit. Tenetur animi excepturi nemo earum
+                adipisci ad fugiad quia tempora expedita alias , blanditiis repellat, ullam numquam voluptates ab debitis laborum.Tempora , ullam.
+                </p>
+                <h3>casemiro</h3>
+                <span>traveler</span>
+                <img src="./assest8/images/casemiro.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                Lorem ipsum dolor , sit amet consectetur adipisicing elit. Tenetur animi excepturi nemo earum
+                adipisci ad fugiad quia tempora expedita alias , blanditiis repellat, ullam numquam voluptates ab debitis laborum.Tempora , ullam.
+                </p>
+                <h3>joa cancelo</h3>
+                <span>traveler</span>
+                <img src="./assest8/images/telemmglpict000281678835transnvbqzqnjv4bqek9vkm18vrkiph9w2gmnk8fchwzrd3tsyoirwpjwdo-1640926992024817912078.jpeg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                Lorem ipsum dolor , sit amet consectetur adipisicing elit. Tenetur animi excepturi nemo earum
+                adipisci ad fugiad quia tempora expedita alias , blanditiis repellat, ullam numquam voluptates ab debitis laborum.Tempora , ullam.
+                </p>
+                <h3>mesut ozil</h3>
+                <span>traveler</span>
+                <img src="./assest8/images/1200px-Mesut_Özil,_Germany_national_football_team_(02).jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                Lorem ipsum dolor , sit amet consectetur adipisicing elit. Tenetur animi excepturi nemo earum
+                adipisci ad fugiad quia tempora expedita alias , blanditiis repellat, ullam numquam voluptates ab debitis laborum.Tempora , ullam.
+                </p>
+                <h3>raphinha</h3>
+                <span>traveler</span>
+                <img src="./assest8/images/raphinha-barca1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                Lorem ipsum dolor , sit amet consectetur adipisicing elit. Tenetur animi excepturi nemo earum
+                adipisci ad fugiad quia tempora expedita alias , blanditiis repellat, ullam numquam voluptates ab debitis laborum.Tempora , ullam.
+                </p>
+                <h3>luke shaw</h3>
+                <span>traveler</span>
+                <img src="./assest8/images/imago1008949249h.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>
+                Lorem ipsum dolor , sit amet consectetur adipisicing elit. Tenetur animi excepturi nemo earum
+                adipisci ad fugiad quia tempora expedita alias , blanditiis repellat, ullam numquam voluptates ab debitis laborum.Tempora , ullam.
+                </p>
+                <h3>ibrahimovic</h3>
+                <span>traveler</span>
+                <img src="./assest8/images/Ibra.jpg" alt="">
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- reviews section ends-->
+
+<!--footer section starts-->
+
+<section class="footer">
+
+    <div class="box-container">
+       
+        <div class="box">
+           <h3>quick links</h3>
+           <a href="home.php"><i class="fas fa-angle-right"></i> home </a>
+           <a href="about.php"><i class="fas fa-angle-right"></i> about </a>
+           <a href="package.php"><i class="fas fa-angle-right"></i> package </a>
+           <a href="book.php"><i class="fas fa-angle-right"></i> book </a>
+        </div>
+
+        <div class="box">
+           <h3>extra links</h3>
+           <a href="#"><i class="fas fa-angle-right"></i> ask questions </a>
+           <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+           <a href="#"><i class="fas fa-angle-right"></i> privacy us</a>
+           <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+           <h3>contact info</h3>
+           <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+           <a href="#"><i class="fas fa-phone"></i> +111-222-3333 </a>
+           <a href="#"><i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+           <a href="#"><i class="fas fa-map"></i> mumbai , india - 400104</a>
+        </div>
+
+        <div class="box">
+           <h3>folilow us</h3>
+           <a href="#"><i class="fab fa-facebook-f"></i> facebook </a>
+           <a href="#"><i class="fab fa-twitter"></i> twitter </a>
+           <a href="#"><i class="fab fa-instagram"></i> instagram </a>
+           <a href="#"><i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>mr.web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!--footer section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--swiper js link-->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!--custom js file link-->
+<script src="./assest8/script/script.js"></script>
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!--swiper css link-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+
+
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <!--custom css file link-->
+    <link rel="stylesheet" href="./assest8/css/style.css">
+
+    <!--custom css responsive file link-->
+    <link rel="stylesheet" href="./assest8/css/responsive.css">
+</head>
+<body>
+    
+<!--header section starts-->
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel</a>
+
+    <nav class="navbar">
+        <ul>
+            <li>
+                <a href="home.php">home</a>
+            </li>
+            <li>
+                <a href="about.php">about</a>
+            </li>
+            <li>
+                <a href="package.php">package</a>
+            </li>
+            <li>
+                <a href="book.php">book</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!--header section ends-->
+
+<div class="heading" style="background: url(assest8/images/image-blue-curve-abstract-illustration-vector-wallpapar-presentation-web-design-modern-poster-business-background-luxu.jpg) no-repeat">
+    <h1>packages</h1>
+</div>
+
+<!--packages section starts-->
+
+<section class="packages">
+
+    <h1 class="heading-title"> top destinations</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/8-15-danh-lam-thang-canh-viet-nam-noi-tieng-nhat-tren-the-gioi.webp" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/Danh-lam-thang-canh-Viet-Nam-dep-nhat-1024x1024.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/nhung-tham-canh-say-dam-long-nguoi-tren-the-gioi_HASZwpdd.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/mua-hoa-man1.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/anh-bia-15-danh-lam-thang-canh-viet-nam-noi-tieng-nhat-tren-the-gioi.webp" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/vietnam-mountain.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/istockphoto-1159222457-612x612.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/desert-va-dessert-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/gettyimages973103362e1545954446452getty-15720503595351086949355.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/fe9724d8fb4da3dd4590353bd771a276.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/contiki-hp-hot-air-balloon-selfie-0277amer2019.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./assest8/images/211011 Resize KV halong-01-min_0.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor , sit amet consectetur adipisicing elit. Pererendis, perspiciatis</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="load-more">
+        <span class="btn">load more</span>
+    </div>
+
+</section>
+
+<!--packages section ends-->
+
+<!--footer section starts-->
+
+<section class="footer">
+
+    <div class="box-container">
+       
+        <div class="box">
+           <h3>quick links</h3>
+           <a href="home.php"><i class="fas fa-angle-right"></i> home </a>
+           <a href="about.php"><i class="fas fa-angle-right"></i> about </a>
+           <a href="package.php"><i class="fas fa-angle-right"></i> package </a>
+           <a href="book.php"><i class="fas fa-angle-right"></i> book </a>
+        </div>
+
+        <div class="box">
+           <h3>extra links</h3>
+           <a href="#"><i class="fas fa-angle-right"></i> ask questions </a>
+           <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+           <a href="#"><i class="fas fa-angle-right"></i> privacy us</a>
+           <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+           <h3>contact info</h3>
+           <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+           <a href="#"><i class="fas fa-phone"></i> +111-222-3333 </a>
+           <a href="#"><i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+           <a href="#"><i class="fas fa-map"></i> mumbai , india - 400104</a>
+        </div>
+
+        <div class="box">
+           <h3>folilow us</h3>
+           <a href="#"><i class="fab fa-facebook-f"></i> facebook </a>
+           <a href="#"><i class="fab fa-twitter"></i> twitter </a>
+           <a href="#"><i class="fab fa-instagram"></i> instagram </a>
+           <a href="#"><i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>mr.web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!--footer section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--swiper js link-->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!--custom js file link-->
+<script src="./assest8/script/script.js"></script>
+</body>
+</html>
